f9c3e3abd674091f074a74a03c3316282687c00d
Danil Anikin <mart4r.clearlist@gmail.com>
2024-09-22 13:10:01 +1000

    Initial Commit

---
+++ AnimeMatch/AnimeMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeMatchApp.swift
+//  AnimeMatch
+//
+//  Created by Данил Аникин on 22.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimeMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
