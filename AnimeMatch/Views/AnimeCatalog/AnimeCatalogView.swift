baa483cad5a810d54372298b76e9bfebb86bfbf8
Danil Anikin <mart4r.clearlist@gmail.com>
2024-09-22 14:58:34 +1000

    Added AnimeCatalogView and AnimeCatalogItemView

---
+++ AnimeMatch/Views/AnimeCatalog/AnimeCatalogView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimeCatalogView.swift
+//  AnimeMatch
+//
+//  Created by Данил Аникин on 22.09.2024.
+//
+
+import SwiftUI
+
+struct AnimeCatalogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimeCatalogView()
+}
